78b9c5c809832f135cf7402e30848987bedcb06d
Kickblip <wyatt.zilker@gmail.com>
2023-12-05 21:40:07 -0700

    day 5 SUCKS

---
+++ day05/part_2.py
@@ -0,0 +1,49 @@
+import os
+
+with open(f'{os.getcwd()}/day05/input.txt', 'r') as f:
+    data = f.read().splitlines()
+
+map = {}
+recent_cat = ''
+categories = []
+
+for line in data:  # populating map
+    if line and not line[0].isnumeric():  # category title
+        cat_title = line.split(':')[0]
+        recent_cat = cat_title
+        categories.append(cat_title)
+
+        map[cat_title] = []
+
+    elif line:  # number map
+        map[recent_cat].append([int(x) for x in line.split(' ')])
+
+
+# creating seed list and formatting the map into intervals
+old_seed_list = [seed for seed in map['seeds']][0]
+seed_list = []
+
+for idx, seed in enumerate(old_seed_list):  # create intervals for seeds
+    if idx % 2 == 0:
+        seed_list.append([seed, seed + old_seed_list[idx + 1] - 1])
+
+for cat_title, lst in map.items():  # create intervals for categories
+    for idx, row in enumerate(lst):
+        lst[idx] = [[row[0], row[0] + (row[2] - 1)],
+                    [row[1], row[1] + (row[2] - 1)]]
+del map['seeds']  # seeds is stored in the seed_list not in the map
+
+print(map)
+
+
+# start with a category and compare the seed interval to the category intervals
+
+def compare_seeds_to_cat(seed_list, category):
+    # cat_lst is a list of 2 subintervals - idx is the row number in the category
+    for idx, cat_lst in enumerate(map[category]):
+        # print(idx, cat_lst)
+        for seed_interval_lst in seed_list:
+            print(seed_interval_lst)
+
+
+ans = compare_seeds_to_cat(seed_list, 'seed-to-soil map')
---
+++ day05/part_2_O.py
@@ -0,0 +1,126 @@
+from typing import List, Set
+import os
+
+
+# Represents one specific map
+class Interval:
+    def __init__(self, start=0, length=0, offset=0, line=''):
+        if len(line) > 0:
+            dest, start, length = [int(x) for x in line.split(' ')]
+            offset = dest - start
+        self.start = start
+        self.end = start + length - 1
+        self.offset = offset
+
+    def contains(self, point: int):
+        return self.start <= point <= self.end
+
+    def map(self, point: int):
+        return point + self.offset
+
+    def outputs(self, mapped_point: int):
+        return self.start + self.offset <= mapped_point <= self.end + self.offset
+
+    def undo_map(self, mapped_point: int):
+        return mapped_point - self.offset
+
+
+class IntervalList:
+    def __init__(self, intervals: List[Interval]):
+        self.intervals = intervals
+
+    def process(self, point: int):
+        for interval in self.intervals:
+            if interval.contains(point):
+                return interval.map(point)
+        return point
+
+    def undo_process(self, outputs: Set[int]):
+        potential_inputs = set()
+        for interval in self.intervals:
+            for output in outputs:
+                if interval.outputs(output):
+                    potential_inputs.add(interval.undo_map(output))
+        outputs.update(potential_inputs)
+
+    def filter(self, points: Set[int]):
+        results = set()
+        for point in points:
+            if any(interval.contains(point) for interval in self.intervals):
+                results.add(point)
+        return results
+
+    def boundaries(self, candidate_points: Set[int]):
+        for interval in self.intervals:
+            candidate_points.add(interval.start)
+            candidate_points.add(interval.end)
+
+
+def parse_input(input):
+    input += '\n'
+    lines = iter(input.split('\n'))
+
+    # Parse the line of seeds
+    seed_input, _ = next(lines).split(': ')[1], next(lines)
+    seeds = [int(seed) for seed in seed_input.split(' ')]
+    seed_intervals = [Interval(seeds[i], seeds[i+1])
+                      for i in range(0, len(seeds), 2)]
+    seed_intervals = IntervalList(seed_intervals)
+
+    # Will contain the seven layers of maps
+    interval_lists: List[IntervalList] = []
+
+    for x in range(7):
+        # Skip the blank line and header lines
+        _, line = next(lines), next(lines)
+
+        intervals = []
+        while len(line) != 0:
+            intervals.append(Interval(line=line))
+            line = next(lines)
+
+        interval_list = IntervalList(intervals)  # One layer of maps
+        interval_lists.append(interval_list)
+
+    return seeds, seed_intervals, interval_lists
+
+
+def part1(seeds: List[int], interval_lists: List[IntervalList]):
+    min_seed = float('inf')
+    for seed in seeds:
+        # Process the seed in each of the seven layers
+        for intervalList in interval_lists:
+            seed = intervalList.process(seed)
+        min_seed = seed if seed < min_seed else min_seed
+    return min_seed
+
+
+def part2(seed_intervals: IntervalList, interval_lists: List[IntervalList]):
+
+    # Set of input seeds which can potentially results in the optimal location
+    candidate_points = set()
+    # Go in reverse order from the last map to the first map
+    for intervalList in interval_lists[::-1]:
+        # Undo the processing for candidate_points from lower maps
+        intervalList.undo_process(candidate_points)
+        # This map layer contributes its boundaries as candidates
+        intervalList.boundaries(candidate_points)
+
+    # Remove candidates that are not valid
+    candidate_points = seed_intervals.filter(candidate_points)
+
+    # Test the candidates and find the optimal one
+    return part1(candidate_points, interval_lists)
+
+
+def solution(input):
+    seeds, seed_intervals, interval_lists = parse_input(input)
+
+    part2_ans = part2(seed_intervals, interval_lists)
+    print(f'Answer: {part2_ans}')
+
+
+with open(f'{os.getcwd()}/day05/input.txt', 'r') as f:
+    data = f.read()
+
+solution(data)
